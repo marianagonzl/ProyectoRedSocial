31f7dd3d8c4da8b3763b2853c9a0963741b9b691
Ana Nicole <21163@portalcedi.edu.mx>
2024-03-19 13:48:50 -0600

    classlista

--- Usuario.h
+++ Usuario.h
@@ -1,1 +1,32 @@
+#ifndef usuario_h
+#define usuario_h
+
+#include <iostream>
+#include "Publicacion.h"
+#include <vector>
+#include <string>
+using namespace std;
+
+class Usuario{
+private: 
+int id;
+public:
+string nombre;
+int edad;
+string nacionalidad;
+Usuario(string nombre);
+Usuario(string nombre, int edad);
+Usuario(string nombre, int edad, string nacionalidad);
+vector<Usuario*> amigos;
+vector<Publicaciones*> publicaciones;
+void mostrar();
+void mostrarAmigos();
+void mostarPublicaciones();
+void agregarAmigo(Usuario* nuevoAmigo);
+void crearPublicacion();
+Usuario* getAmigo(int id);
+
+
+};
+#endif
 
