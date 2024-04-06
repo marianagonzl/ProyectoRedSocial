c85ee9cd1206d06bef7c66c0ccf70602aae53166
anannicole <21163@portalcedi.edu.mx>
2024-04-06 10:51:03 -0600

    cambios

--- Usuario.h
+++ Usuario.h
@@ -24,40 +24,37 @@ void mostrar();
 void mostrarAmigos();
 void mostarPublicaciones();
 void agregarAmigo(Usuario* nuevoAmigo);
-void crearPublicacion(string& fecha, string& contenido);
+void crearPublicacion(string fecha, string contenido);
 Usuario* getAmigo(int id);
-Usuario(int id, string& nombre):
-     id(id), nombre(nombre) {};
-Usuario(int id,string& nombre, int edad):
-    id(id), nombre(nombre), edad(edad) {};
-Usuario(string nombre, int edad, string nacionalidad):
- id(id), nombre(nombre), edad(edad), nacionalidad(nacionalidad){};
+Usuario(string nombre);
+Usuario(string nombre, int edad);
+Usuario(string nombre, int edad, string nacionalidad);
 
 
 
 
 
 int Usuario::getId(){
-    return id;
+    return this->id;
 }
 
 void Usuario::mostrar(){
-cout<< "ID: "<<id<<endl;
-cout<< "Nombre: "<<nombre<<endl;
-cout<< "Edad: "<<edad<<endl;
-cout<< "Nacionalidad: "<<nacionalidad<<endl;
+cout<< "ID: "<<this->id<<endl;
+cout<< "Nombre: "<<this->nombre<<endl;
+cout<< "Edad: "<<this->edad<<endl;
+cout<< "Nacionalidad: "<<this->nacionalidad<<endl;
 }
 
 void Usuario::mostrarAmigos(){
     cout << "Amigo de " << nombre << " : " <<endl;
-    for(size_t i = 0; i < amigos.size(); ++i) {
+    for(int i = 0; i < amigos.size(); i++) {
         amigos[i]->mostrar();
     }
 }
 
 void Usuario::mostrarPublicaciones(){
     cout << "publicacion " << nombre << " : " << endl;
-    for (size_t i = 0; i < publicaciones.size(); ++i) {
+    for (int i = 0; i < publicaciones.size(); i++) {
         publicaciones[i]->mostrarPublicacion();
     }
 }
@@ -66,12 +63,12 @@ void Usuario::agregarAmigo(Usuario* nuevoAmigo){
     nuevoAmigo -> amigos.push_back(this);
 
 }
-void Usuario::crearPublicacion(string& fecha, string& contenido){
+void Usuario::crearPublicacion(string fecha, string contenido){
     Publicacion* nuevaPublicacion = new Publicacion(this, fecha, contenido);
     publicaciones.push_back(nuevaPublicacion);
 }
  Usuario::Usuario* getAmigo(int id){
-for(size_t i = 0; i < amigos.size(); ++i) {
+for(int i = 0; i < amigos.size(); i++) {
     if(amigos[i]->getId() == id) {
         return amigos[i];
     }
